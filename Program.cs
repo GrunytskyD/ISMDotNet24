568e784f1c113ba1aabc3d2412df9b1198904763
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 22:11:02 +0300

    Значення дискримінанту

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("A");
+            double numbera = double.Parse(Console.ReadLine());
+            Console.WriteLine("B");
+            double numberb = double.Parse(Console.ReadLine());
+            Console.WriteLine("С");
+            double numberc = double.Parse(Console.ReadLine());
+            double D = numberb * numberb - (4 * numbera * numberc);
+            Console.WriteLine($"D={D}");
+        }
+    }
+}
